629ff2c01eb84bb2bbed3c3371e5b0110d425778
Yutang Xie <pikachuhappy2019@gmail.com>
2020-10-24 22:19:07 -0700

    Added divmod

--- homework/10-25-2020/wash_hands.py
+++ homework/10-25-2020/wash_hands.py
@@ -12,6 +12,18 @@ def wash_hands(times_washed, num_months_followed):
     seconds = total_washed_seconds - minutes * 60
     return f"{minutes} minutes and {seconds} seconds"
 
+def wash_hands_divmod(times_washed, num_months_followed):
+    """
+    >>> wash_hands(8, 7)
+    '588 minutes and 0 seconds'
+    >>> wash_hands(0, 0)
+    '0 minutes and 0 seconds'
+    >>> wash_hands(7, 9)
+    '661 minutes and 30 seconds'
+    """
+    minutes, seconds = divmod((times_washed * num_months_followed) * 21 * 30, 60)
+    return f"{minutes} minutes and {seconds} seconds"
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod(verbose=True)
