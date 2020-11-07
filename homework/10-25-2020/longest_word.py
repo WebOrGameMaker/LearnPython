9c4c60ede3162f6d44d67a82fab2139b000e5ff3
Yutang Xie <pikachuhappy2019@gmail.com>
2020-11-06 20:46:03 -0800

    Made some changes

--- homework/10-25-2020/longest_word.py
+++ homework/10-25-2020/longest_word.py
@@ -16,6 +16,30 @@ def find_longest_word(string):
             largest_word = word
     return largest_word
 
-if __name__ == "__main__":
+def find_longest_word2(sentence):
+    """
+    >>> find_longest_word2("a b c de")
+    'de'
+    >>> find_longest_word2("a bc def ghij klmno pqrstu vwxyz")
+    'pqrstu'
+    >>> find_longest_word2("a dog is something")
+    'something'
+    >>> find_longest_word2("")
+    ''
+    """
+    word_start = 0
+    longest_word_start, longest_word_end = 0, 1
+    max_length = 0
+    for i in range(len(sentence) + 1):
+        if i == len(sentence) or sentence[i] == " ":
+            word_length = i - word_start
+            if word_length > max_length:
+                max_length = word_length
+                longest_word_start = word_start
+                longest_word_end = i
+            word_start = i + 1
+    return sentence[longest_word_start: longest_word_end]
+
+if __name__ == '__main__':
     import doctest
     doctest.testmod(verbose=True)
