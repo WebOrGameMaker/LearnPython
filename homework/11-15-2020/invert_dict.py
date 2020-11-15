7cfcc8c580a64c14fcac674dbd049834a38e2859
Yutang Xie <pikachuhappy2019@gmail.com>
2020-11-15 09:30:08 -0800

    30 mins before class

---
+++ homework/11-15-2020/invert_dict.py
@@ -0,0 +1,17 @@
+def invert(d):
+    """
+    >>> invert({ 'z': 'q', 'w': 'f'})
+    {'q': 'z', 'f': 'w'}
+    >>> invert({ 'a': 1, 'b': 2, 'c': 3})
+    {1: 'a', 2: 'b', 3: 'c'}
+    >>> invert({'zebra': 'koala', 'horse': 'camel'})
+    {'koala': 'zebra', 'camel': 'horse'}
+    """
+    d_copy = d.copy()
+    d.clear()
+    for key, value in d_copy.items():
+        d[value] = key
+    return d
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
---
+++ homework/11-15-2020/sum_of_two_numbers_with_a_twist.py
@@ -0,0 +1,78 @@
+# I know, this isn't allowed
+def hard_add1(num1, num2):
+    """
+    >>> hard_add1("5125515215521515", "125261616261626")
+    '5250776831783141'
+    >>> hard_add1("6666666666666666666666666666", "99999999999999999999999")
+    '6666766666666666666666666665'
+    >>> hard_add1("123456789123456789123456789", "987654321987654321987654329876543")
+    '987654445444443445444443453333332'
+    """
+    return str(int(num1) + int(num2))
+
+def hard_add2(s1, s2):
+    """
+    >>> hard_add2("5125515215521515", "125261616261626")
+    '5250776831783141'
+    >>> hard_add2("6666666666666666666666666666", "99999999999999999999999")
+    '6666766666666666666666666665'
+    >>> hard_add2("123456789123456789123456789", "987654321987654321987654329876543")
+    '987654445444443445444443453333332'
+    >>> hard_add2("99", "9")
+    '108'
+    """
+    i = -1
+    carry = 0
+    digit1 = 0
+    digit2 = 0
+    result = [None] * (max(len(s1), len(s2)) + 1)
+    while len(s1) >= -i or len(s2) >= -i:
+        if len(s1) >= -i:
+            digit1 = int(s1[i])
+        else:
+            digit1 = 0
+        if len(s2) >= -i:
+            digit2 = int(s2[i])
+        else:
+            digit2 = 0
+        digit_sum = digit1 + digit2 + carry
+        if digit_sum >= 10:
+            carry = 1
+            result[i] = str(digit_sum - 10)
+        else:
+            carry = 0
+            result[i] = str(digit_sum)
+        i -= 1
+    if carry == 1:
+        result[0] = str(carry)
+    else:
+        result[0] = '' 
+    return "".join(result)
+
+def hard_pythonic_version_by_dad(s1, s2):
+    """
+    >>> hard_pythonic_version_by_dad("5125515215521515", "125261616261626")
+    '5250776831783141'
+    >>> hard_pythonic_version_by_dad("6666666666666666666666666666", "99999999999999999999999")
+    '6666766666666666666666666665'
+    >>> hard_pythonic_version_by_dad("123456789123456789123456789", "987654321987654321987654329876543")
+    '987654445444443445444443453333332'
+    >>> hard_pythonic_version_by_dad("99", "9")
+    '108'
+    """
+    carry, digit1, digit2 = 0, 0, 0
+    i = -1
+    result = [None] * (max(len(s1), len(s2)) + 1)
+    while len(s1) >= -i or len(s2) >= -i:
+        digit1 = int(s1[i]) if len(s1) >= -i else 0
+        digit2 = int(s2[i]) if len(s2) >= -i else 0
+        digit_sum = digit1 + digit2 + carry
+        result[i] = str(digit_sum - 10) if digit_sum >= 10 else str(digit_sum)
+        carry = 1 if digit_sum >= 10 else 0
+        i -= 1
+    result[0] = "1" if carry == 1 else ''
+    return "".join(result)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
