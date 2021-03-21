e8d6edfdb6d828ca17c6f6c1be7f36fab15479aa
Yutang Xie <pikachuhappy2019@gmail.com>
2021-03-21 10:07:13 -0700

    FIXED

--- homework/2021-03-21/third_largest.py
+++ homework/2021-03-21/third_largest.py
@@ -3,12 +3,12 @@ def third_largest(nums):
     >>> third_largest([2, 4, 7, 3, 10, 4, 9, 23])
     9
     >>> third_largest([1])
-    -1
+    1
     """
+    first = nums[0]
+    second = nums[0]
+    third = nums[0]
     if len(nums) > 3:
-        first = nums[0]
-        second = nums[0]
-        third = nums[0]
         for num in nums:
             if num > first:
                 first, second, third = num, first, second
@@ -18,7 +18,7 @@ def third_largest(nums):
                 third = num
         return third
     else:
-        return -1
+        return first
 
 if __name__ == '__main__':
     import doctest
