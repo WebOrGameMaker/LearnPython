b12635b959a54eb80c1d6730663568ad2f77f431
Yutang Xie <pikachuhappy2019@gmail.com>
2021-05-15 20:50:45 -0700

    OK. (but it's fixed)

--- homework/2021-05-16/selection_sort.py
+++ homework/2021-05-16/selection_sort.py
@@ -1,10 +1,21 @@
+
+
 def selection_sort(nums):
+    """
+    >>> selection_sort([1, 3, 5, 7, 9, 2, 4, 6, 8, 10])
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
     for i in range(len(nums) - 1):
         minimum = i
         for j in range(i + 1, len(nums)):
             if nums[j] < nums[minimum]:
-                nums[j] = minimum
-                
+                minimum = j
+
+        # Remember, this is still in the 1st for loop, so it continually switches.
         if minimum != i:
             nums[minimum], nums[i] = nums[i], nums[minimum]
     return nums
+
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod(verbose=True)
