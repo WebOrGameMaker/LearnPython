7540c89fbd4255a312ef9cb2975c8093b37860c6
Yutang Xie <pikachuhappy2019@gmail.com>
2021-06-12 21:04:01 -0700

    D O C T E S T

--- homework/2021-06-13/recursve_factoral.py
+++ homework/2021-06-13/recursve_factoral.py
@@ -1,8 +1,16 @@
 def Factorial(n):
+    """
+    >>> Factorial(0)
+    1
+    >>> Factorial(1)
+    1
+    >>> Factorial(5)
+    120
+    """
     if n > 1:
         return n * Factorial(n-1)
     return 1
 
-print(Factorial(0)) # 0! = 1
-print(Factorial(1))
-print(Factorial(5))
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod(verbose=True)
